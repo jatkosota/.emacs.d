a2c109f2a506de1b33e25bd59169732debff1836
jatkosota <jatkosota.mika@gmail.com>
2016-08-15 01:39:17 +0900

    add init.el

---
+++ init.el
@@ -0,0 +1,33 @@
+;; PATH問題の解決
+(defun set-exec-path-from-shell-PATH ()
+  "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell.
+This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
+  (interactive)
+  (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+    
+(set-exec-path-from-shell-PATH)
+
+;; el-get
+(when load-file-name
+  (setq user-emacs-directory (file-name-directory load-file-name)))
+
+(add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+;; use-package
+(el-get-bundle use-package)
+(require 'use-package)
+
+;; init-loader
+(el-get-bundle init-loader)
+(use-package init-loader
+  :config
+  (setq init-loader-show-log-after-init nil)
+  (init-loader-load "~/.emacs.d/inits"))
