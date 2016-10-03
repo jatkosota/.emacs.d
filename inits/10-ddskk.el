23d112057335ef2b34e51736d37ae66b3856a7ea
jatkosota <jatkosota.mika@gmail.com>
2016-10-03 17:13:39 +0900

    change punctuation

--- inits/10-ddskk.el
+++ inits/10-ddskk.el
@@ -3,5 +3,5 @@
   :bind (("C-x j" . skk-mode))
   :init
   (setq skk-isearch-start-mode 'latin)
-  (setq-default skk-kutouten-type '(". " . ", "))
+  (setq-default skk-kutouten-type '("．" . "，"))
   (setq skk-dcomp-active t))
